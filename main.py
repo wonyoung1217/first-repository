e1838cecc1abcc9b9bc46ceaeb52a51de7642ded
wonyoung1217 <petercwy@naver.com>
2022-09-23 16:26:57 +0900

    Create hello_world.py

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import max_profit as mp
 import prime_number as pn
 
 if __name__ == '__main__':
-    pn.is_prime_number(100)
+    pn.is_prime_number(1)
     pn.is_prime_number(13)
-    pn.is_prime_number(100)
+    pn.is_prime_number(24)
     pn.is_prime_number(31)
     pn.is_prime_number(45)
-    pn.is_prime_number(100)
+    pn.is_prime_number(97)
 
